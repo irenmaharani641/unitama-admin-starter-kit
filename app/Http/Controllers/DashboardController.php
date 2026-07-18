22737d2134c1929958be28832c6ab9cec8438580
Iren Maharani Ruben <irenmaharani641@gmail.com>
2026-07-18 07:50:24 +0700

    Pertemuan 15: Final Version

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardController extends Controller
 {
@@ -17,46 +20,87 @@ class DashboardController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+   
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
+   
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show()
     {
-        //
+        return view('dashboard.show',[
+            'title' => 'Detail User',
+            'user' => Auth::user(),
+            
+
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit()
     {
-        //
+            return view('dashboard.edit',[
+            'title' => 'Edit User',
+            'user' => Auth::user(),
+            
+
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+    public function update(Request $request)
+     {
+     $user = Auth::user();
+    $validated = $request->validate([
+    'name'                  => 'required|string|max:255',
+    'email'                 => 'required|string|email|max:255|unique:users,email', 
+    'password'              => 'required|string|min:8',
+    'password_confirmation' => 'required|same:password',
+    'avatar'                => 'nullable|image|mimes:jpeg,png,jpg|max:1048', 
+    
+], [
+    
+    'name.required'                  => 'Nama tidak boleh kosong.',
+    'name.max'                       => 'Nama maksimal 255 karakter.',   
+    'email.required'                 => 'Email tidak boleh kosong.',
+    'email.email'                    => 'Format email tidak valid.',
+    'email.unique'                   => 'Email sudah terdaftar.',
+    'password.required'              => 'Password tidak boleh kosong.',
+    'password.min'                   => 'Password minimal harus 8 karakter.',
+    'password_confirmation.required' => 'Konfirmasi password tidak boleh kosong.',
+    'password_confirmation.same'     => 'Konfirmasi password harus sama dengan password.',
+    'avatar.image'                   => 'Berkas harus berupa gambar.',
+    'avatar.mimes'                   => 'Format gambar harus jpeg, png, atau jpg.',
+    'avatar.max'                     => 'Ukuran gambar maksimal adalah 1 MB (1048 KB).',
+    
+]);
+    DB::beginTransaction();
+    try {
 
+    if($request->file('avatar')) {
+    $validated['avatar'] = $request->file('avatar')->store('avatar', 'public');
+}
+
+
+    $validated['password'] = bcrypt($request->password);
+    $validated['email_verified_at'] = now();
+
+        $user->update($validated);
+        DB::commit();
+        return to_route('dashboard.show')->withSuccess('Data Berhasil Diupdate');
+    } catch (\Exception $e) {
+        DB::rollBack();
+        dd($e->getMessage());
+        return to_route('dashboard.edit')->withError('Data Gagal Diupdate');
+    }
+    }
+    
+    
+    
     /**
      * Remove the specified resource from storage.
      */
@@ -64,4 +108,6 @@ class DashboardController extends Controller
     {
         //
     }
-}
+
+    }
+    
